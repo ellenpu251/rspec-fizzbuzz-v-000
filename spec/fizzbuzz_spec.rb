77859573173d79a1945a3c4ab8df026c72434fb5
Ellen Pu <ellenpu251@gmail.com>
2019-01-15 07:19:05 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,7 +4,7 @@ require_relative '../fizzbuzz.rb'
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  if int % 5 == 0
     "Buzz"
   elsif (int % 3 == 0 && int % 5 == 0)
     "FizzBuzz"
